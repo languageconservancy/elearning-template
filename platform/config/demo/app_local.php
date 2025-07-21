2eb20c56f8ba44d859ab98884f5a3c603c2ea388
Pete Vieira <pete.vieira@gmail.com>
2025-07-21 10:27:24 -0700

    Update to use app_local.php for non-secret config, and .env (not source controlled) secrets

---
+++ platform/config/demo/app_local.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'debug' => true,
+
+    'App.name' => 'eLearning Demo',
+    'App.author' => 'The Language Conservancy',
+
+    'Datasources.default.username' => 'root',
+    'Datasources.default.password' => 'root',
+    'Datasources.default.database' => 'elearning_demo_db',
+
+    // Displayed in various places in the user interface.
+    'LANGUAGE' => 'Demo Language',
+
+    // Upload settings
+    'SITEUPLOAD' => true,
+
+    // Amazon Web Services settings
+    'AWSUPLOAD' => false,
+    'AWSBUCKETNAME' => '',
+    'AWSREGION' => '',
+    'AWS_LINK' => '',
+
+    // Links to the frontend and backend
+    'FROENTEND_LINK' => 'http://localhost:4200/',
+    'ADMIN_LINK' => 'http://localhost/backend/',
+
+    // Teacher Portal settings
+    'CLASSROOMPATHID' => 0,
+    'ALLUNITSLEVELID' => 0,
+];
---
+++ platform/config/production/app_local.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'debug' => true,
+
+    'App.name' => 'eLearning Production',
+    'App.author' => 'The Language Conservancy',
+
+    'Datasources.default.database' => 'elearning_production_db',
+
+    // Displayed in various places in the user interface.
+    'LANGUAGE' => 'Production Language',
+
+    // Upload settings
+    'SITEUPLOAD' => true,
+
+    // Amazon Web Services settings
+    'AWSUPLOAD' => false,
+    'AWSBUCKETNAME' => '',
+    'AWSREGION' => '',
+    'AWS_LINK' => '',
+
+    // Links to the frontend and backend
+    'FROENTEND_LINK' => 'http://localhost:4200/',
+    'ADMIN_LINK' => 'http://localhost/backend/',
+
+    // Teacher Portal settings
+    'CLASSROOMPATHID' => 0,
+    'ALLUNITSLEVELID' => 0,
+];
